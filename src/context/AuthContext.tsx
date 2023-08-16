ec52a2c6160726d7f7fab379420bd9b60c9cb41d
Terence Dumas <dumast@awty.org>
2023-08-16 01:04:22 -0500

    finished design

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -9,6 +9,8 @@ import { doc, getDoc, getFirestore, onSnapshot } from "firebase/firestore";
 import firebase_app from '@/firebase/config';
 
 import type { _AuthContext, _DbUser } from '@/utils/types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 const auth = getAuth(firebase_app);
 const db = getFirestore(firebase_app)
@@ -30,39 +32,36 @@ export const AuthContextProvider = ({
     useEffect(() => {
         return onIdTokenChanged(auth, async (user: User | null) => {
             setUserLoading(true);
+            setDbUserLoading(true);
             if (!user) {
                 setUser(null);
+                setDbUser(null);
+                setDbUserLoading(false);
                 nookies.set(undefined, 'token', '', { path: '/' });
             } else {
                 const token = await user.getIdToken();
                 setUser(user);
                 nookies.set(undefined, 'token', token, { path: '/' });
+                onSnapshot(doc(db, "users", user.uid), (doc) => {
+                    const dbUserData = doc.data();
+                    if (dbUserData != undefined && dbUserData != null) {
+                        setDbUser({
+                            displayName: dbUserData.displayName || null,
+                            username: dbUserData.username,
+                            email: dbUserData.email,
+                            friends: dbUserData.friends
+                        })
+                    }
+                    setDbUserLoading(false);
+                })
             }
             setUserLoading(false);
         });
     }, []);
 
-    useEffect(() => {
-        if (user != null) {
-            return onSnapshot(doc(db, "users", user.uid), (doc) => {
-                setDbUserLoading(true);
-                const dbUserData = doc.data();
-                if (dbUserData != undefined && dbUserData != null) {
-                    setDbUser({
-                        displayName: dbUserData.displayName || null,
-                        username: dbUserData.username,
-                        email: dbUserData.email,
-                        friends: dbUserData.friends
-                    })
-                }
-                setDbUserLoading(false);
-            })
-        }
-    }, [user])
-
     return (
         <AuthContext.Provider value={{ user, dbUser }}>
-            {userLoading || dbUserLoading ? <div>Loading</div> : children}
+            {userLoading || dbUserLoading ? <div className={"main loader"}><FontAwesomeIcon icon={faCircleNotch} size='2xl' /></div> : children}
         </AuthContext.Provider>
     );
 };
